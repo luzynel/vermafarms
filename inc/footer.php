83040be2198a0d9acb8f483acb1780e6816921b6
Ma.Luzynel Rendon <lenyzulmazyn@gmail.com>
2022-04-19 13:15:08 +0800

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,49 @@
+<section class="f_section9">
+  <div class="f_container">
+    <div class="p_prefooter">
+      <div class="c_btn__back"><a href="">BACK TO TOP</a></div>
+      <img src="img/round-footer-logo.png" alt="round-footer-logo">
+      <div class="c_btn__shop"><a href="">Our Shop <img src="img/arrow.png" width="60px" alt="arrow"></a></div>
+    </div>
+  </div>
+</section>
+<footer class="p_footer">
+  <div class="f_container">
+    <ul class="p_footer__nav">
+      <li><a href="">Shop</a></li>
+      <li><a href="">Our Story</a></li>
+      <li><a href="">Learn</a></li>
+      <li><a href="">Community</a></li>
+      <li><a href="">Blog</a></li>
+      <li><a href="">Cart</a></li>
+    </ul>
+    <div class="p_footer__logo"><a href=""><img src="img/logo-white-footer.png" alt="Verma Farms"></a></div>
+    <ul class="p_footer__nav is_small">
+      <li><a href="">Disclaimer</a></li>
+      <li><a href="">Privacy Policy</a></li>
+      <li><a href="">Terms and Conditions</a></li>
+      <li><a href="">Site Credits</a></li>
+    </ul>
+  </div>
+</footer>
+
+
+</div> <!-- End of page_wrapper -->
+
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/plugins.js"></script>
+<script>
+  $(document).ready(function () {
+    lottie.loadAnimation({
+      container: document.getElementById('hero-banner-wave'), // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'js/data-color-1.json' // the path to the animation json
+    });
+  });
+</script>
+</body>
+</html>
+<!-- End Footer -->
---
+++ inc/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Verma Farms</title>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:title" content="">
+  <meta property="og:image" content="img_ogp.png">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="">
+
+  <link rel="icon" href="/favicon.ico">
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/common_header.css">
+  <link rel="stylesheet" href="css/common_footer.css">
+</head>
+
+<body>
+  <div class="f_page">
+
+  <!-- Header -->
+  <header class="p_header">
+    <h1 class="p_header__logo"><a href="#"><img src="img/logo.svg" alt="Verma Farms"></a></h1>
+    <div class="p_header__side">
+      <nav class="p_nav">
+        <ul>
+          <li><a href="">Shop</a></li>
+          <li><a href="">Our Story</a></li>
+          <li><a href="">Learn</a></li>
+        </ul>
+      </nav>
+      <nav class="p_nav">
+        <ul>
+          <li><a href="">Community</a></li>
+          <li><a href="">Blog</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="p_cart"><a href="">1</a></div>
+  </header>
