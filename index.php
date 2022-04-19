a1364bc659e9c74fc1e4bfacfb598c16a7bb069b
Ma.Luzynel Rendon <lenyzulmazyn@gmail.com>
2022-04-19 13:15:41 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,327 @@
+<?php
+  set_include_path ('inc');
+  include "header.php"; 
+?>
+
+  <section class="c_banner">
+    <img src="img/homepage-bg.png" alt="">
+    <div class="c_banner__wrap">      
+      <div class="f_container">
+        <div class="c_banner__content">
+          <h2 class="c_ttl is_large">Pure All Natural CBD Inspire by the Islands of Hawaii</h2>
+          <p>ZERO THC - all natural cannabidoil</p>
+          <div class="c_btn is_banner"><a href="">Shop Now</a></div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="p_benefits">
+    <div class="f_container">
+      <h2 class="c_ttl">Verma's Benefits</h2>
+      <div class="c_content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor.</p>
+      </div>
+      <div class="p_benefits__thumb">
+        <div class="p_benefits__box">
+          <img src="img/image1.png" alt="Quiet your mind and calm the storm.">
+          <div class="p_benefits__ttl">Quiet your mind and calm the storm.</div>
+        </div>
+        <div class="p_benefits__box">
+          <img src="img/image2.png" alt="Ease your pain and soothe your joints.">
+          <div class="p_benefits__ttl">Ease your pain and soothe your joints.</div>
+        </div>
+        <div class="p_benefits__box">
+          <img src="img/image3.png" alt="Sleep tight and wake up refreshed.">
+          <div class="p_benefits__ttl">Sleep tight and wake up refreshed.</div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="p_about">
+    <div class="f_container">
+      <h2 class="c_ttl"><img src="img/round-logo-orange.png" width="58px" alt="About Our CBD">About Our CBD</h2>
+      <div class="c_content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Quisque scelerisque fermentum bibendum.</p>
+      </div>
+      <div class="p_about__content">
+        <div class="p_about__column">
+          <div class="p_about__box">
+            <img src="img/icons/ingredients-icon.svg" width="100px" alt="High Quality Ingredients">
+            <h3>High Quality Ingredients</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim, tristique ac pulvinar eget, lacinia sed turpis.</p>
+          </div>
+          <div class="p_about__box">
+            <img src="img/icons/benefit-icon.svg" width="60px" alt="xyz Benefit">
+            <h3>xyz Benefit</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis <br>tortor.</p>
+          </div>
+        </div>
+        <div id="hero-banner-wave" class="p_about__slider">          
+          <div class="p_about__wrap">
+            <img src="img/white-bowl.png" width="400px" alt="white-bowl">
+            <img src="img/weed-bowl-placeholder.png" alt="weed-bowl-placeholder" style="width: 450px; clip-path: inset(0px 40% 0px 0px); left: -75px;">
+            <img src="img/gummies-bowl.png" alt="gummies-bowl" style="width: 380px; clip-path: inset(0px 0px 0px 50%);">
+          </div>
+        </div>
+        <div class="p_about__column">
+          <div class="p_about__box">
+            <img src="img/icons/thc-icon.svg" width="63px" alt="THC">
+            <h3>THC</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim.</p>
+          </div>
+          <div class="p_about__box">
+            <img src="img/icons/plants-icon.svg" width="81px" alt="From Plants… to Gummies">
+            <h3>From Plants… to Gummies</h3>
+            <p>Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim.</p>
+          </div>
+        </div>
+      </div>
+      <div class="c_btn is_green"><a href="">MORE ABOUT CBD</a></div>
+    </div>
+  </section>
+
+  <section class="f_section__card p_handoil">
+    <div class="f_container">
+      <div class="c_card">
+        <h2 class="c_ttl"><small>Satisfaction Guaranteed</small>Love It, Or It’s On Us</h2>
+        <ul class="p_handoil__box">
+          <li>
+            <img src="img/icons/hands-icon.svg" width="66px" alt="Try Verma Farms Risk Free">
+            <div>
+              <h3>Try Verma Farms Risk Free</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur. </p>
+            </div>
+          </li>
+          <li>
+            <img src="img/icons/squiggles-icon.svg" width="52x" alt="No High Whatsoever">
+            <div>
+              <h3>No High Whatsoever</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur. </p>
+            </div>
+          </li>
+          <li>
+            <img src="img/icons/lemon-icon.svg" width="49px" alt="All Natural Ingredients">
+            <div>
+              <h3>All Natural Ingredients</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur. </p>
+            </div>
+          </li>
+          <li>
+            <img src="img/icons/legal-icon.svg" width="55px" alt="Now Legal In All 50 States">
+            <div>
+              <h3>Now Legal In All 50 States</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur. </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+
+  <section class="f_section__card p_gummies">
+    <img class="p_gummies__bg1" src="img/palm-left.png" width="380px" alt="palm-left">
+    <img class="p_gummies__bg2" src="img/palm-right.png" width="250px" alt="palm-right">
+    <div class="f_container">
+      <img class="p_gummies__img" src="img/gummies-stacked.png" width="680px" alt="gummies-stacked">
+      <div class="c_card is_medium is_right">
+        <h2 class="c_ttl"><small>How Does It Work?</small>It’s Simple, Really</h2>
+        <div class="c_content">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Quisque scelerisque fermentum bibendum.</p>
+        </div>
+        <div class="c_btn"><a href="">LEARN MORE</a></div>
+      </div>
+    </div>
+  </section>
+
+  <section class="f_section6">
+    <div class="f_container">
+
+      <h2 class="c_ttl">Our Products</h2>
+
+      <div class="p_product">
+
+        <div class="p_product__main">
+          <div class="p_product__img--large"><img src="img/products/beary-beach.png" width="384px" alt="Beary Beach CBD Gummies"></div>
+          <div class="p_product__detail">
+            <h3>Hawaiian Rainbow CBD Gummies</h3>
+            <div class="p_product__content">
+              <div class="is_size">10g CBD</div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim.</p>
+              <div class="is_price">$39.99</div>
+            </div>
+            <div class="p_product__btn"><a href="">VIEW</a></div>
+          </div>
+        </div>
+
+        <div class="p_product__list">
+          <ul>
+            <li>
+              <a href="">
+                <div class="p_product__img"><img src="img/products/hawaiian-rainbow.png" width="80px" alt="hawaiian-rainbow"></div>
+                <div class="p_product__ttl">Beary Beach CBD Gummies</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="p_product__img"><img src="img/products/maui-melon.png" width="80px" alt="maui-melon"></div>
+                <div class="p_product__ttl">Beary Beach CBD Gummies</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="p_product__img"><img src="img/products/beary-beach.png" width="80px" alt="beary-beach"></div>
+                <div class="p_product__ttl">Beary Beach CBD Gummies</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="p_product__img"><img src="img/products/peachy-pau-hana.png" width="80px" alt="peachy-pau-hana"></div>
+                <div class="p_product__ttl">Beary Beach CBD Gummies</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="p_product__img"><img src="img/products/blueberry-wave.png" width="80px" alt="blueberry-wave"></div>
+                <div class="p_product__ttl">Beary Beach CBD Gummies</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="p_product__img"><img src="img/products/wahoo-worm.png" width="80px" alt="wahoo-worm"></div>
+                <div class="p_product__ttl">Beary Beach CBD Gummies</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="p_product__img"><img src="img/products/island-apple.png" width="80px" alt="island-apple"></div>
+                <div class="p_product__ttl">Beary Beach CBD Gummies</div>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <div class="p_product__img"><img src="img/products/tropical-cherry.png" width="80px" alt="tropical-cherry"></div>
+                <div class="p_product__ttl">Beary Beach CBD Gummies</div>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="c_btn"><a href="">ALL PRODUCTS</a></div>
+
+      </div>
+    </div>
+  </section>
+  
+  <section class="f_section7">
+    <div class="f_container">
+      <div class="p_company">
+        <div class="p_company__column">
+          <div class="p_company__box">            
+            <p>Lorem ipsum dolor sit amet…consectetur.</p>
+            <img src="img/forbes-logo.png" width="126px" alt="forbes-logo">
+          </div>
+          <div class="p_company__box">
+            <p>Nullam rhoncus auctor arcu, nec mollis tortor. Lorem ipsum dolor.</p>
+            <img src="img/hackernoon-logo.png" width="261px" alt="hackernoon-logo">
+          </div>
+        </div>
+        <div class="p_company__img">          
+          <div class="p_company__wrap">
+            <img src="img/potted-plant.png" width="347px" alt="potted-plant">
+          </div>
+        </div>
+        <div class="p_company__column">
+          <div class="p_company__box">
+            <p>Nullam rhoncus auctor arcu, nec mollis tortor. Lorem ipsum dolor.</p>
+            <img src="img/influencive-logo.png" width="195px" alt="influencive-logo">
+          </div>
+          <div class="p_company__box">
+            <p>Lorem ipsum dolor sit amet…consectetur.</p>
+            <img src="img/entrepreneur-logo.png" width="175px" alt="entrepreneur-logo">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="f_section8">
+    <div class="f_container">
+      <h2 class="c_ttl">What People<br>Say About Our Products</h2>
+      <div class="p_testimonial">
+        <div class="p_testimonial__wrap">
+          <div class="p_testimonial__box">
+            <img src="img/oils-product.png" width="180px" alt="oils-product">
+            <div class="p_testimonial__text">
+              <h3>Peachy Pau Hana CBD Oils</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim, tristique ac pulvinar eget, lacinia sed turpis.</p>
+            </div>
+            <div class="p_testimonial__name">
+              <img src="img/round-logo-green.png" alt="round-logo-green">
+              <span>Marlon - Los Angeles, CA</span>
+            </div>
+          </div>
+          <div class="p_testimonial__box">
+            <img src="img/oils-product.png" width="180px" alt="oils-product">
+            <div class="p_testimonial__text">
+              <h3>Peachy Pau Hana CBD Oils</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim, tristique ac pulvinar eget, lacinia sed turpis.</p>
+            </div>
+            <div class="p_testimonial__name">
+              <img src="img/round-logo-green.png" alt="round-logo-green">
+              <span>Marlon - Los Angeles, CA</span>
+            </div>
+          </div>
+          <div class="p_testimonial__box">
+            <img src="img/oils-product.png" width="180px" alt="oils-product">
+            <div class="p_testimonial__text">
+              <h3>Peachy Pau Hana CBD Oils</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim, tristique ac pulvinar eget, lacinia sed turpis.</p>
+            </div>
+            <div class="p_testimonial__name">
+              <img src="img/round-logo-green.png" alt="round-logo-green">
+              <span>Marlon - Los Angeles, CA</span>
+            </div>
+          </div>
+          <div class="p_testimonial__box">
+            <img src="img/oils-product.png" width="180px" alt="oils-product">
+            <div class="p_testimonial__text">
+              <h3>Peachy Pau Hana CBD Oils</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim, tristique ac pulvinar eget, lacinia sed turpis.</p>
+            </div>
+            <div class="p_testimonial__name">
+              <img src="img/round-logo-green.png" alt="round-logo-green">
+              <span>Marlon - Los Angeles, CA</span>
+            </div>
+          </div>
+          <div class="p_testimonial__box">
+            <img src="img/oils-product.png" width="180px" alt="oils-product">
+            <div class="p_testimonial__text">
+              <h3>Peachy Pau Hana CBD Oils</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim, tristique ac pulvinar eget, lacinia sed turpis.</p>
+            </div>
+            <div class="p_testimonial__name">
+              <img src="img/round-logo-green.png" alt="round-logo-green">
+              <span>Marlon - Los Angeles, CA</span>
+            </div>
+          </div>
+          <div class="p_testimonial__box">
+            <img src="img/oils-product.png" width="180px" alt="oils-product">
+            <div class="p_testimonial__text">
+              <h3>Peachy Pau Hana CBD Oils</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus auctor arcu, nec mollis tortor. Mauris enim enim, tristique ac pulvinar eget, lacinia sed turpis.</p>
+            </div>
+            <div class="p_testimonial__name">
+              <img src="img/round-logo-green.png" alt="round-logo-green">
+              <span>Marlon - Los Angeles, CA</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+  <?php include "footer.php"; ?>
+
